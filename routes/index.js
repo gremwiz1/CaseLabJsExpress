15aa5133541d5f6841c97a91cd32d29adc54303d
gremwiz1 <gremwiz@yandex.ru>
2022-06-30 18:47:28 +0300

    update readme

--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,7 @@ router.post("/api/signup", celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
-    name: Joi.string().min(2).max(30),
+    name: Joi.string().min(2).max(30).required(),
   }),
 }), createUser);
 router.use(auth);
